2fc2b6015a09c98c7d2be69b72d3fd4d0d7633ee
malathi <shettymalathi113@gmail.com>
2025-03-20 15:38:49 +0530

    second commit

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -7,19 +7,17 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-    features = "src/test/java/features",
-    glue = {"stepDefinitions"},
-    	//	 tags = "@AddPlace",  // Single tag
- //   tags="@Regression",
-    	//	tags = "@DeletePlace", // compile test verify
-   // tags = "@AddPlace and @DeletePlace",  // Multiple tags using logical operator
-    plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber.json", "json:target/jsonReports/cucumber-report.json"}
-)
+@CucumberOptions(features = "src/test/java/features", glue = { "stepDefinitions" },
+		// tags = "@AddPlace", // Single tag
+		// tags="@Regression",
+		// tags = "@DeletePlace", // compile test verify
+		// tags = "@AddPlace and @DeletePlace", // Multiple tags using logical operator
+		plugin = { "pretty", "html:target/cucumber-report.html", "json:target/cucumber.json",
+				"json:target/jsonReports/cucumber-report.json" })
 public class TestRunner {
-	
-	  @Test
-	    public void simpleTest() {
-	        assertTrue(true);
-}
+
+	@Test
+	public void simpleTest() {
+		assertTrue(true);
+	}
 }
